0b2154ed1b077b4f2ddac5168c9d6742ed8e85a6
saviorocha <42saviorocha@gmail.com>
2022-10-01 18:51:57 -0300

    Fixed (finally) plot bug of messing the axes when inserting a vector bigger than five  units

--- components/d3/plotComponent.ts
+++ components/d3/plotComponent.ts
@@ -4,6 +4,8 @@ class PlotComponent {
   svg;
   vectors: VectorData[][];
   margin: Margin;
+  width: number;
+  height: number;
 
   // @ts-ignore
   x: ScaleLinear<number, number>;
@@ -21,92 +23,84 @@ class PlotComponent {
     const { margin, width, height } = dimensions;
     this.vectors = vectors;
     this.margin = margin;
+    this.width = width;
+    this.height = height;
 
     this.svg = d3
       .select(refComponent)
       .attr("id", "svgPlot")
       .append("svg:svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
+      .attr("width", this.width + this.margin.left + this.margin.right)
+      .attr("height", height + this.margin.top + this.margin.bottom)
       .append("g")
       .attr("id", "plane")
-      .attr("transform", `translate(${margin.left},${margin.top})`);
-    this.createAxis(width, height);
-    this.createChart();
+      .attr("transform", `translate(${this.margin.left},${this.margin.top})`);
+    // this.createAxis(width, height);
+    this.createPlot();
 
     // that's not how I wanted to deal with events
     // but it's the solution I've got so far (￢_￢;)
     if (eventsArr.length) {
       this.addEvents(eventsArr);
     } else {
-      this.createZoom(width, height, margin);
+      this.createZoom();
     }
     // @ts-ignore
     this.createLine(this.x, this.y);
     this.createVector();
   }
 
-  createAxis = (width: number, height: number) => {
-    // Initialise a X axis:
-    this.x = d3.scaleLinear().range([0, width]);
+  /**
+   * Initializes axis values and adds them to the svg to create the plot
+   */
+  createPlot = () => {
+    // initialize a X axis:
+    this.x = d3.scaleLinear().range([0, this.width]);
     this.xAxis = d3.axisBottom(this.x);
     this.svg
       .append("g")
-      .attr("transform", `translate(0, ${height / 2})`)
+      .attr("transform", `translate(0, ${this.height / 2})`)
       .attr("id", "myXaxis");
-    // Initialize an Y axis
-    this.y = d3.scaleLinear().range([height, 0]);
+
+    // initialize an Y axis
+    this.y = d3.scaleLinear().range([this.height, 0]);
     this.yAxis = d3.axisLeft(this.y);
     this.svg
       .append("g")
-      .attr("transform", `translate(${width / 2}, 0)`)
+      .attr("transform", `translate(${this.width / 2}, 0)`)
       .attr("id", "myYaxis");
-  };
 
-  createChart = () => {
+    // sets the domain
     const defaultMax = 5;
     const defaultMin = -5;
-    const maxX = d3.max(this.vectors, function (data) {
-      return data[1].coord1;
-    });
-    const minX = d3.min(this.vectors, function (data) {
-      return data[1].coord1;
-    });
-    const maxY = d3.max(this.vectors, function (data) {
-      return data[1].coord2;
-    });
-    const minY = d3.min(this.vectors, function (data) {
-      return data[1].coord2;
-    });
+    this.x.domain([defaultMin, defaultMax]);
+    this.y.domain([defaultMin, defaultMax]);
 
-    // Create the X axis:
-    this.x.domain([
-      minX && minX < defaultMin ? minX : defaultMin,
-      maxX && maxX > defaultMax ? maxX : defaultMax,
-    ]);
+    // add the axis
     this.svg.selectAll("#myXaxis").call(this.xAxis);
-    // create the Y axis
-    this.y.domain([
-      minY && minY < defaultMin ? minY : defaultMin,
-      maxY && maxY > defaultMax ? maxY : defaultMax,
-    ]);
     this.svg.selectAll("#myYaxis").call(this.yAxis);
   };
 
+  /**
+   * Executes a set of functions to add events to the plot svg
+   * @param {EventFunction[]} eventsArr
+   */
   addEvents = (eventsArr: EventFunction[]) => {
     for (let i = 0; i < eventsArr.length; i++) {
       eventsArr[i]();
     }
   };
 
-  // Sets the zoom and pan features: how much you can zoom, on which part, and what to do when there is a zoom
-  createZoom = (width: number, height: number, margin: Margin) => {
+  /**
+   * Sets the zoom and pan features: how much you can zoom, on which part, and what to do when there is a zoom
+   */
+  createZoom = () => {
     const zoom: ZoomBehavior<any, unknown> = d3
       .zoom()
       .scaleExtent([0.5, 20]) // This control how much you can unzoom (x0.5) and zoom (x20)
       .extent([
         [0, 0],
-        [width, height],
+        [this.width, this.height],
       ])
       .on("zoom", this.handleVectorZoom);
 
@@ -116,36 +110,61 @@ class PlotComponent {
     this.svg
       .append("rect")
       .attr("id", "zoom-rect")
-      .attr("width", width)
-      .attr("height", height)
+      .attr("width", this.width)
+      .attr("height", this.height)
       .style("fill", "none")
       .style("pointer-events", "all")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+      .attr(
+        "transform",
+        "translate(" + this.margin.left + "," + this.margin.top + ")"
+      )
       .call(zoom);
   };
 
-  // Function that updates the chart when the user zoom and thus new boundaries are available
+  /**
+   *  Updates the chart values when the user zoom and thus new boundaries are available
+   */
   handleVectorZoom = (event: any) => {
     // recover the new scale
     const newX = event.transform.rescaleX(this.x);
     const newY = event.transform.rescaleY(this.y);
 
-    let newDomain = [-180, 0, 180, 360];
+    // new domain values calculated, based on the dimension,
+    // to be used on the svg's translate attribute
+    let newDomain = [
+      -(this.height - this.height / 2),
+      0,
+      this.height - this.height / 2,
+      this.height,
+    ];
+    // let newDomain = [-180, 0, 180, 360];
     newDomain = newDomain.map((n) => {
       return event.transform.k * n;
     });
 
-    let xRange = [0, 180, 360, 540];
+    let xRange = [
+      0,
+      this.height / 2,
+      this.height,
+      this.height + this.height / 2,
+    ];
+    // let xRange = [0, 180, 360, 540];
     xRange = xRange.map((n) => {
       return event.transform.k * n;
     });
 
-    let yRange = [10, 190, 370, 550];
+    let yRange = [
+      10,
+      this.height / 2 + 10,
+      this.height + 10,
+      this.height + this.height / 2 + 10,
+    ];
+    // let yRange = [10, 190, 370, 550];
     yRange = yRange.map((n) => {
       return event.transform.k * n;
     });
 
-    // scales used to be able to move the axes along with the vectors
+    // scales used on the translate to be able to move the axes along with the vectors
     const xAxisScale = d3.scaleLinear().domain(newDomain).range(xRange);
     const yAxisScale = d3.scaleLinear().domain(newDomain).range(yRange);
 
@@ -163,6 +182,11 @@ class PlotComponent {
     this.createLine(newX, newY);
   };
 
+  /**
+   * Draws a D3 line to represent a vector, using the vector's coordinates data
+   * @param x the class x scale function
+   * @param y the class y scale function
+   */
   createLine = (
     x: ScaleLinear<number, number>,
     y: ScaleLinear<number, number>
@@ -194,7 +218,9 @@ class PlotComponent {
       .attr("marker-end", "url(#arrow)");
   };
 
-  // adds the vector's arrow to the line element
+  /**
+   * Adds the vector's arrow to the line element
+   */
   createVector = () => {
     let def = this.svg
       .append("defs")
