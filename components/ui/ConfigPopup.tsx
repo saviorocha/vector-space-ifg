bee227aef5997fa8fef1d5f9f0c9f6c0d5d2ea37
saviorocha <42saviorocha@gmail.com>
2022-11-01 21:57:11 -0300

    Refactor of poput components

--- components/ui/ConfigPopup.tsx
+++ components/ui/ConfigPopup.tsx
@@ -1,17 +1,116 @@
-import React, { FunctionComponent, useState } from "react";
-import { Settings } from "react-feather";
-import { PropsChildren } from "../../interfaces/interfaces";
-import styles from "../../styles/modules/config.module.css";
+import { DarkModeToggle, Mode } from "@anatoliygatt/dark-mode-toggle";
+import { useTheme } from "next-themes";
+import { useState } from "react";
+import { Feather, Globe, Hash, Moon } from "react-feather";
+import { useConfigContext } from "../../context/ConfigContext";
+import stylesconfig from "../../styles/modules/config.module.css";
+import PopupWindow from "./PopupWindow";
 
-const ConfigPopup: FunctionComponent<PropsChildren> = ({ children }) => {
-  const [isActive, setIsActive] = useState(false);
+const ConfigPopup = () => {
+  const {
+    hideNumbers,
+    setHideNumbers,
+    showBasisVectors,
+    setShowBasisVectors,
+    decimalPoint,
+    setDecimalPoint,
+    showGridLines,
+    setShowGridLines,
+  } = useConfigContext();
+  const { theme, setTheme } = useTheme();
+  const [mode, setMode] = useState<Mode>("dark");
+
   return (
-    <>
-      {isActive && <section className={styles.configpopup}>{children}</section>}
-      <button className="z-10" onClick={() => setIsActive(!isActive)}>
-        <Settings size={35} />
-      </button>
-    </>
+    <PopupWindow>
+      <ul className="mt-3 ">
+        <li className={stylesconfig.popupitem}>
+          <div className={stylesconfig.configicon}>
+            <Moon />
+          </div>
+          <p>Tema</p>
+          <div className={stylesconfig.configcontrol}>
+            <DarkModeToggle
+              mode={mode}
+              size="sm"
+              inactiveTrackColor="#e2e8f0"
+              inactiveTrackColorOnHover="#f8fafc"
+              inactiveTrackColorOnActive="#cbd5e1"
+              activeTrackColor="#334155"
+              activeTrackColorOnHover="#1e293b"
+              activeTrackColorOnActive="#0f172a"
+              inactiveThumbColor="#1e293b"
+              activeThumbColor="#e2e8f0"
+              onChange={(mode) => {
+                setMode(mode);
+                setTheme(theme === "dark" ? "light" : "dark");
+              }}
+            />
+          </div>
+        </li>
+        <li className={stylesconfig.popupitem}>
+          <div className={stylesconfig.configicon}>
+            <Globe />
+          </div>
+          <p>Mostrar linhas grid</p>
+          <div className={stylesconfig.configcontrol}>
+            <input
+              type="checkbox"
+              checked={showGridLines}
+              onChange={() => {
+                setShowGridLines(!showGridLines);
+              }}
+            />
+          </div>
+        </li>
+        <li id="toggle-numbers" className={stylesconfig.popupitem}>
+          <div className={stylesconfig.configicon}>
+            <Hash />
+          </div>
+          <p>Mostrar números</p>
+          <div className={stylesconfig.configcontrol}>
+            <input
+              type="checkbox"
+              checked={hideNumbers}
+              onChange={() => {
+                setHideNumbers(!hideNumbers);
+              }}
+            />
+          </div>
+        </li>
+        <li className={stylesconfig.popupitem}>
+          <div className={stylesconfig.configicon}>
+            <Feather />
+          </div>
+          <p>Configurar casas decimais</p>
+          <div className={stylesconfig.configcontrol}>
+            <input
+              type="number"
+              className="w-10 h-5 mt-2"
+              value={decimalPoint}
+              onChange={(event: any) => {
+                setDecimalPoint(event.target.value);
+              }}
+              min={0}
+            />
+          </div>
+        </li>
+        <li className={stylesconfig.popupitem}>
+          <div className={stylesconfig.configicon}>
+            <Feather />
+          </div>
+          <p>Incluir vetores canônicos</p>
+          <div className={stylesconfig.configcontrol}>
+            <input
+              type="checkbox"
+              checked={showBasisVectors}
+              onChange={() => {
+                setShowBasisVectors(!showBasisVectors);
+              }}
+            />
+          </div>
+        </li>
+      </ul>
+    </PopupWindow>
   );
 };
 
