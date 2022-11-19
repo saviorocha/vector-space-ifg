842cb2bdf578553c229d662cc1d2fdb0362ce094
saviorocha <42saviorocha@gmail.com>
2022-11-18 23:19:39 -0300

    Animation page responsivness

--- components/pagesections/animationPage/MainSectionAnimationPage.tsx
+++ components/pagesections/animationPage/MainSectionAnimationPage.tsx
@@ -1,21 +1,17 @@
 import { Tooltip } from "@mui/material";
 import Link from "next/link";
 import { CornerUpLeft } from "react-feather";
-import styles from "../../../styles/modules/pages/editpage.module.css";
+import styles from "../../../styles/modules/pages/animation.module.css";
 import D3PlotAnimation from "../../d3/D3plotAnimation";
 import ConfigPopup from "../../ui/inputs/ConfigPopup";
 
 const MainSectionAnimationPage = () => {
   return (
     <main
-      // className="
-      //   mx-auto absolute h-3/4 right-0
-      //   flex justify-center items-center
-      // "
       className="mx-auto"
       id={styles.main}
     >
-      <section id={styles.return} className="absolute top-5 left-5">
+      <section id={styles.return} className="absolute top-5 left-5 z-10">
         <Tooltip title="Voltar para criação de transformações">
           <Link href="/transformacaolinear/editartransformacoes">
             <a id={styles.returnbtn}>
@@ -25,7 +21,6 @@ const MainSectionAnimationPage = () => {
         </Tooltip>
       </section>
       <section
-        id={styles.middlesection}
         // className="h-full flex items-center justify-center"
         className="
           relative gap-1 overflow-hidden
--- pages/transformacaolinear/animartransformacoes.tsx
+++ pages/transformacaolinear/animartransformacoes.tsx
@@ -1,26 +1,6 @@
-import { useRouter } from "next/router";
-import { LegacyRef, useEffect, useRef, useState } from "react";
-import { Transition } from "react-transition-group";
-import D3PlotAnimation from "../../components/d3/D3plotAnimation";
+import { useEffect } from "react";
 import MainSectionAnimationPage from "../../components/pagesections/animationPage/MainSectionAnimationPage";
-import SideBarAnimationPage from "../../components/pagesections/animationPage/SideBarAnimationPage";
 import { useD3Context } from "../../context";
-
-// transition properties of the main component
-const mainTransitionStyles: any = {
-  entering: { width: "100%" },
-  entered: { width: "80%" },
-  exiting: { width: "80%" },
-  exited: { width: "100%" },
-};
-
-// transition properties of the sidebar component
-const sideBarTransitionStyles: any = {
-  entering: { width: "0px", padding: "0px" },
-  entered: { width: "20%" },
-  exiting: { width: "20%" },
-  exited: { width: "0px", padding: "0px" },
-};
 
 const AnimationPlane = () => {
   const { setEvents } = useD3Context();
@@ -31,11 +11,6 @@ const AnimationPlane = () => {
 
   return (
     <div className="h-screen">
-      <SideBarAnimationPage
-        sideBarStyle={{
-          width: "450px",
-        }}
-      />
       <MainSectionAnimationPage />
     </div>
   );
